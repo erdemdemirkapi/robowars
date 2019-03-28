6adf729e49a5b6fee79ff9b3d191da1b49c58e77
erdem <demirkapierdem@gmail.com>
2019-03-28 11:30:40 +0300

    added damage and energy calculation

--- robot.py
+++ robot.py
@@ -1,6 +1,7 @@
 class Robot:
 
-    def __init__(self, ability, energy, location):
+    def __init__(self, name, ability, energy, location):
+        self.name = name
         self.ability = ability
         self.energy = energy
         self.location = location
--- robowars.py
+++ robowars.py
@@ -1,10 +1,42 @@
 from robot import Robot
 from ability import Ability
+import random
 
 ability1 = Ability(8, 1, 1)
 ability2 = Ability(5, 3, 2)
 
-robot1 = Robot(ability1, 100, 0)
-robot2 = Robot(ability2, 100, 19)
+robot1 = Robot('robot1', ability1, 100, 0)
+robot2 = Robot('robot2', ability2, 100, 19)
+
+i = 0
+while(robot1.energy > 0 and robot2.energy > 0):
+    i += 1
+    #DAMAGE(R2) = ( RANDOM(BETWEEN 1 AND 10) * A(R1) / DISTANCE ) – (D(R2) * (DISTANCE / 10))
+
+    distance = abs(robot1.location - robot2.location)
+
+    damage_r2 = (random.randint(0, 10) * robot1.ability.attack / distance) - (robot2.ability.defence * (distance / 10))
+    if damage_r2 < 0:
+        damage_r2 = 0
+    
+    robot2.energy -= damage_r2
+    print(robot2.energy)
+
+    damage_r1 = (random.randint(0, 10) * robot2.ability.attack / distance) - (robot1.ability.defence * (distance / 10))
+    if damage_r1 < 0:
+        damage_r1 = 0
+
+    robot1.energy -= damage_r1
+    print(robot1.energy)
 
 
+    #robot1.energy = 0
+
+print('total step: ', i)
+
+
+
+if robot1.energy <= 0:
+    print('Winner: ', robot2.name)
+elif robot2.energy <= 0:
+    print('Winner: ', robot1.name)
