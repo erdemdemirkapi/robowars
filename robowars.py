832f5950c29b73d80e3ded7d24ad2a6fc6e5307c
erdemdemirkapi <demirkapierdem@gmail.com>
2019-03-30 00:56:54 +0300

    continue to gui

--- robowars.py
+++ robowars.py
@@ -24,6 +24,66 @@ def move(robot, location_of_other_robot):
     robot.energy -= consume_energy
     robot.location = new_location
 
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height = 400, width = 700)
+canvas.pack()
+
+enter_ability_label = tk.Label(root, text = "Enter abilities: ")
+enter_ability_label.place(x = 10, y = 40)
+
+attack_label = tk.Label(root, text = "Attack")
+attack_label.place(x = 120, y = 5)
+attack_input1 = tk.Entry(width = 5)
+attack_input1.place(x = 120, y = 25)
+
+defence_label = tk.Label(root, text = "Defence")
+defence_label.place(x = 190, y = 5)
+defence_input1 = tk.Entry(width = 5)
+defence_input1.place(x = 190, y = 25)
+
+speed_label = tk.Label(root, text = "Speed")
+speed_label.place(x = 260, y = 5)
+speed_input1 = tk.Entry(width = 5)
+speed_input1.place(x = 260, y = 25)
+
+attack_label = tk.Label(root, text = "Attack")
+attack_label.place(x = 120, y = 60)
+attack_input2 = tk.Entry(width = 5)
+attack_input2.place(x = 120, y = 60)
+
+defence_label = tk.Label(root, text = "Defence")
+defence_label.place(x = 190, y = 60)
+defence_input2 = tk.Entry(width = 5)
+defence_input2.place(x = 190, y = 60)
+
+speed_label = tk.Label(root, text = "Speed")
+speed_label.place(x = 260, y = 60)
+speed_input2 = tk.Entry(width = 5)
+speed_input2.place(x = 260, y = 60)
+
+def set_robot_ability():
+    attack1_string = attack_input1.get()
+    attack1_int = int(attack1_string)
+    defence1_string = defence_input1.get()
+    defence1_int = int(defence1_string)
+    speed1_string = speed_input1.get()
+    speed1_int = int(speed1_string)
+
+    attack2_string = attack_input2.get()
+    attack2_int = int(attack2_string)
+    defence2_string = defence_input2.get()
+    defence2_int = int(defence2_string)
+    speed2_string = speed_input2.get()
+    speed2_int = int(speed2_string)
+    
+    ability1 = Ability(attack1_int, defence1_int, speed1_int)
+    ability2 = Ability(attack2_int, defence2_int, speed2_int)
+    
+    return ability1, ability2
+
+save_button = tk.Button(root, text ="Save", command = set_robot_ability, bg = '#80c1ff')
+save_button.place(x = 330, y = 40)
 
 ability1 = Ability(6, 2, 2)
 ability2 = Ability(5, 3, 2)
@@ -53,56 +113,5 @@ if robot1.energy <= 0:
 elif robot2.energy <= 0:
     print('Winner: ', robot1.name)
 
-
-root = tk.Tk()
-
-canvas = tk.Canvas(root, height = 400, width = 700)
-canvas.pack()
-
-enter_ability_label = tk.Label(root, text = "Enter abilities: ")
-enter_ability_label.place(x = 10, y = 28)
-
-enter_ability_label = tk.Label(root, text = "Attack")
-enter_ability_label.place(x = 120, y = 5)
-attack_input = tk.Entry(width = 5)
-attack_input.place(x = 120, y = 25)
-
-enter_ability_label = tk.Label(root, text = "Defence")
-enter_ability_label.place(x = 190, y = 5)
-defence_input = tk.Entry(width = 5)
-defence_input.place(x = 190, y = 25)
-
-enter_ability_label = tk.Label(root, text = "Speed")
-enter_ability_label.place(x = 260, y = 5)
-speed_input = tk.Entry(width = 5)
-speed_input.place(x = 260, y = 25)
-
-
-def helloCallBack():
-    a = attack_input.get()
-    print(a)
-
-save_button = tk.Button(root, text ="Save", command = helloCallBack, bg = '#80c1ff')
-save_button.place(x = 330, y = 25)
-
-
-
-# background_label = tk.Label(root)
-# background_label.place(relwidth=1, relheight=1)
-
-# frame = tk.Frame(root, bg='#80c1ff', bd=5)
-# frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
-
-# entry = tk.Entry(frame, font=40)
-# entry.place(relwidth=0.65, relheight=1)
-
-# button = tk.Button(frame, text="Get Weather", font=40, command=lambda: get_weather(entry.get()))
-# button.place(relx=0.7, relheight=1, relwidth=0.3)
-
-# lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
-# lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
-
-# label = tk.Label(lower_frame)
-# label.place(relwidth=1, relheight=1)
 
 root.mainloop()
