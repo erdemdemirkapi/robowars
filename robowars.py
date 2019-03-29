81e2b901f5c5357d100dbf5d1f8aaf4ec6960fa2
erdem <demirkapierdem@gmail.com>
2019-03-29 18:06:02 +0300

    add gui

--- robowars.py
+++ robowars.py
@@ -2,6 +2,8 @@ from robot import Robot
 from ability import Ability
 import random
 
+import tkinter as tk
+
 def distance(d1, d2):
     return abs(d1 - d2)
 
@@ -21,7 +23,6 @@ def move(robot, location_of_other_robot):
     consume_energy = (distance(robot.location, new_location) / robot.ability.speed) * 5
     robot.energy -= consume_energy
     robot.location = new_location
-
 
 
 ability1 = Ability(6, 2, 2)
@@ -51,3 +52,57 @@ if robot1.energy <= 0:
     print('Winner: ', robot2.name)
 elif robot2.energy <= 0:
     print('Winner: ', robot1.name)
+
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height = 400, width = 700)
+canvas.pack()
+
+enter_ability_label = tk.Label(root, text = "Enter abilities: ")
+enter_ability_label.place(x = 10, y = 28)
+
+enter_ability_label = tk.Label(root, text = "Attack")
+enter_ability_label.place(x = 120, y = 5)
+attack_input = tk.Entry(width = 5)
+attack_input.place(x = 120, y = 25)
+
+enter_ability_label = tk.Label(root, text = "Defence")
+enter_ability_label.place(x = 190, y = 5)
+defence_input = tk.Entry(width = 5)
+defence_input.place(x = 190, y = 25)
+
+enter_ability_label = tk.Label(root, text = "Speed")
+enter_ability_label.place(x = 260, y = 5)
+speed_input = tk.Entry(width = 5)
+speed_input.place(x = 260, y = 25)
+
+
+def helloCallBack():
+    a = attack_input.get()
+    print(a)
+
+save_button = tk.Button(root, text ="Save", command = helloCallBack, bg = '#80c1ff')
+save_button.place(x = 330, y = 25)
+
+
+
+# background_label = tk.Label(root)
+# background_label.place(relwidth=1, relheight=1)
+
+# frame = tk.Frame(root, bg='#80c1ff', bd=5)
+# frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+
+# entry = tk.Entry(frame, font=40)
+# entry.place(relwidth=0.65, relheight=1)
+
+# button = tk.Button(frame, text="Get Weather", font=40, command=lambda: get_weather(entry.get()))
+# button.place(relx=0.7, relheight=1, relwidth=0.3)
+
+# lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
+# lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+
+# label = tk.Label(lower_frame)
+# label.place(relwidth=1, relheight=1)
+
+root.mainloop()
